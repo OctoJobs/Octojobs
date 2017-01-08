4d68c8124e50020d19d075807683f65f21844ed7
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-08 10:23:07 -0800

    updating routes to include about.jinja2 template

--- octojobs/octojobs/routes.py
+++ octojobs/octojobs/routes.py
@@ -4,3 +4,4 @@
 def includeme(config):
     config.add_static_view(name='static', path='octojobs:static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('about', '/about')
