0273ff156fae0e7b8096f473edfa3e4d7cd85517
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-08 09:36:58 -0800

    setting up routes to static files

--- octojobs/octojobs/__init__.py
+++ octojobs/octojobs/__init__.py
@@ -5,6 +5,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.add_static_view(name='static', path='octojobs:static')
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
--- octojobs/octojobs/routes.py
+++ octojobs/octojobs/routes.py
@@ -1,3 +1,3 @@
 def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view(name='static', path='octojobs:static', cache_max_age=3600)
     config.add_route('home', '/')
