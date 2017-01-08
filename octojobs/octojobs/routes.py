f5400910bf28d4014a833cdbe44fc95a6f505cd2
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-08 09:40:54 -0800

    adding docstring on routes page

--- octojobs/octojobs/routes.py
+++ octojobs/octojobs/routes.py
@@ -1,3 +1,6 @@
+"""Adding routes for the configuration to find."""
+
+
 def includeme(config):
     config.add_static_view(name='static', path='octojobs:static', cache_max_age=3600)
     config.add_route('home', '/')
