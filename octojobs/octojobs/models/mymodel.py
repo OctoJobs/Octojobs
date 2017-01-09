dd6ca645073a6510e2bc1c11968d8200f3c99c9f
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-09 15:55:36 -0800

    started adding one-to-many relationsship

--- octojobs/octojobs/models/mymodel.py
+++ octojobs/octojobs/models/mymodel.py
@@ -5,21 +5,32 @@ from sqlalchemy import (
     Unicode,
     Date,
     Point,
-    Boolean
+    Boolean,
+    ForeignKey
 )
 
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from .meta import Base
 
+Base = declarative_base
 
 class Language(Base):
     __tablename__ = 'programming_language'
     id = Column(Integer, primary_key=True)
-    version_id = Column(Integer, foreign_key = True)
+    children = relationship("Job", back_populates="parent")
+    version_id = Column(Integer)
+    name = Column(Unicode)
+
+class Version(Base):
+    __tablename__ = 'programming_language'
+    id = Column(Integer, primary_key=True)
     name = Column(Unicode)
 
 class City(Base):
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True)
+    children = relationship("Job", back_populates="parent")
     country_id = Column(Integer, foreign_key=True)
     name = Column(Unicode)
     long_lat = Column(Point)
@@ -27,6 +38,7 @@ class City(Base):
 class Employer(Base):
     __tablename__ = 'employer'
     id = Column(Integer, primary_key=True)
+    children = relationship("Job", back_populates="parent")
     city_id = Column(Integer, foreign_key=True)
     name = Column(Unicode)
     reputation = Column(Integer)
@@ -34,6 +46,7 @@ class Employer(Base):
 class Website(Base):
     __tablename__ = 'website'
     id = Column(Integer, primary_key=True)
+    children = relationship("Job", back_populates="parent")
     name = Column(Unicode)
     url = Column(Unicode)
     refresh_rate = Column(Integer)
@@ -43,6 +56,7 @@ class Website(Base):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    children = relationship("Job", back_populates="parent")
     firstname = Column(Unicode)
     lastname = Column(Unicode)
     email = Column(Unicode)
@@ -51,18 +65,27 @@ class User(Base):
     favorites = Column(Blob)
     csv = Column(Blob)
 
+class Currency(Base):
+    __tablename__ = 'currency'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode)
+    country_id = Column(Unicode)
+
 class Job(Base):
     __tablename__ = 'job'
     id = Column(Integer, primary_key=True)
-    version_id = Column(Integer, foreign_key=True)
-    city_id = Column(Integer, foreign_key=True)
-    employer_id = Column(Integer, foreign_key=True)
-    source_id = Column(Integer, foreign_key=True)
-    currency_id = Column(Integer, foreign_key=True)
-    language_id = Column(Integer, foreign_key=True)
+    # version_id = Column(Integer, foreign_key=True)
+    city_id = Column(Integer, ForeignKey('city.id'))
+    parent = relationship('City', back_populates='children')
+    employer_id = Column(Integer, ForeignKey('employer.id'))
+    parent = relationship('Employer', back_populates='children')
+    source_id = Column(Integer, ForeignKey('website.id'))
+    currency_id = Column(Integer, ForeignKey('currency.id')
+    language_id = Column(Integer, ForeignKey('language_id))
+    parent = relationship('Language', back_populates='children')
     creation_date = Column(Date)
     update_date = Column(Date)
-    tile = Column(Integer)
+    title = Column(Integer)
     description = Column(Integer)
     salary = Column(Integer)
     sponsors_visa = Column(Boolean)
