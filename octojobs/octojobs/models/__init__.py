4b699d5b943481d69ce3e8712139c023d527ea81
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-09 16:45:14 -0800

    changed reference to MyModel

--- octojobs/octojobs/models/__init__.py
+++ octojobs/octojobs/models/__init__.py
@@ -5,7 +5,7 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .mymodel import MyModel  # noqa
+from .mymodel import Language, City, Employer, Website, User, Job  # noqa
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
