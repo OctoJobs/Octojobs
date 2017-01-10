8d34cfd4c018f04ba95430a0245a337030491f0f
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-09 16:14:46 -0800

    fixed parent/children relationships

--- octojobs/octojobs/models/mymodel.py
+++ octojobs/octojobs/models/mymodel.py
@@ -18,13 +18,13 @@ Base = declarative_base
 class Language(Base):
     __tablename__ = 'programming_language'
     id = Column(Integer, primary_key=True)
-    children = relationship("Job", back_populates="parent")
+    job = relationship("Job", back_populates="parent")
     name = Column(Unicode)
 
 class City(Base):
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True)
-    children = relationship("Job", back_populates="parent")
+    job = relationship("Job", back_populates="parent")
     country_id = Column(Integer, foreign_key=True)
     name = Column(Unicode)
     long_lat = Column(Point)
@@ -32,7 +32,7 @@ class City(Base):
 class Employer(Base):
     __tablename__ = 'employer'
     id = Column(Integer, primary_key=True)
-    children = relationship("Job", back_populates="parent")
+    job = relationship("Job", back_populates="parent")
     city_id = Column(Integer, foreign_key=True)
     name = Column(Unicode)
     reputation = Column(Integer)
@@ -40,7 +40,7 @@ class Employer(Base):
 class Website(Base):
     __tablename__ = 'website'
     id = Column(Integer, primary_key=True)
-    children = relationship("Job", back_populates="parent")
+    job = relationship("Job", back_populates="parent")
     name = Column(Unicode)
     url = Column(Unicode)
     refresh_rate = Column(Integer)
@@ -50,26 +50,26 @@ class Website(Base):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    children = relationship("Job", back_populates="parent")
+    job = relationship("Job", back_populates="parent")
     firstname = Column(Unicode)
     lastname = Column(Unicode)
     email = Column(Unicode)
     passphrase = Column(Unicode)
     last_seen = Column(Date)
     favorites = Column(Blob)
-    csv = Column(Blob)
+    cv = Column(Blob)
 
 class Job(Base):
     __tablename__ = 'job'
     id = Column(Integer, primary_key=True)
     city_id = Column(Integer, ForeignKey('city.id'))
-    parent = relationship('City', back_populates='children')
+    city = relationship('City', back_populates='children')
     employer_id = Column(Integer, ForeignKey('employer.id'))
-    parent = relationship('Employer', back_populates='children')
+    employer = relationship('Employer', back_populates='children')
     source_id = Column(Integer, ForeignKey('website.id'))
-    parent = relationship('Website', back_populates='children')
+    website = relationship('Website', back_populates='children')
     language_id = Column(Integer, ForeignKey('language_id'))
-    parent = relationship('Language', back_populates='children')
+    language = relationship('Language', back_populates='children')
     creation_date = Column(Date)
     update_date = Column(Date)
     title = Column(Integer)
