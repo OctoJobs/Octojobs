b6643b92cb092740abad061254d3b68e76d5f281
Colin Lamont <colinlamont@gmail.com>
2017-01-10 10:39:33 -0800

    Added detail and list info to company_dict for internal and externally listed jobs.

--- octojobs/octojobs/octopus/octopus/spiders/spider.py
+++ octojobs/octojobs/octopus/octopus/spiders/spider.py
@@ -22,65 +22,57 @@ class JobSpider(scrapy.Spider):
         If not, grab the link and go to that page and run again.
         Otherwise, yield the data we want as JSON."""
 
+        company_dict = {}
         if not response.xpath('//*[@id="job-content"]'):
             if response.xpath('//*[@id="resultsCol"]'):
-                url_list = []
-                title_list = []
-                company_list = []
+                # url_list = []
+                # title_list = []
+                # company_list = []
 
                 for element in response.css('div.result h2.jobtitle'):
-                    # import pdb;pdb.set_trace()
+                    
                     anchor = element.css('a.turnstileLink').extract_first()
                     title = re.search(r'title="([^"]*)"', anchor).group(1)
-                    title_list.append(title)
 
-                    url = re.search(r'href="([^"]*)"', anchor).group(1)
                     base_url = 'https://www.indeed.com'
-                    url_list.append(base_url + url)
+                    url = base_url + re.search(r'href="([^"]*)"', anchor).group(1)
+                    company_dict[url] = {}
+                    company_dict[url]['url'] = url
+                    company_dict[url]['title'] = title
 
-                for data in response.css('div.result span.company'):
+                    data = response.css('div.result span.company')
+                    # import pdb;pdb.set_trace()
                     if data.css('span a'):
                         company = data.css('span a::text').extract_first()
-                    company = data.css('span::text').extract_first()
-                    company_list.append(company)
+                    else:
+                        company = data.css('span::text').extract_first()
+                    company = ' '.join(company.split())
+                    company_dict[url]['company'] = company
 
-                for url in url_list:
-                    temp_url = ""
-                    temp_title = ""
-                    temp_company = ""
-
-                    temp_url = url
-                    temp_title = title
-                    temp_company = company
-
-                    url_list.pop(0)
-                    title_list.pop(0)
-                    company_list.pop(0)
-                    yield scrapy.Request(temp_url)
+                for key in company_dict:
+                    yield scrapy.Request(key)
 
                     if not response.xpath('//*[@id="job-content"]'):
-                        yield {
-                            'title': temp_title,
-                            'url': temp_url,
-                            'company': temp_company,
-                            # location = element.css('span.location span::text').extract_first()
-                            # summary = element.css('span.summary::text').extract_first()
-                            # anchor = element.css('h2.jobtitle a.turnstileLink').extract_first()
-                        }
-                        continue
-                    yield scrapy.Request(base_url)
-            else:
-                self.parse(response)
-        elif response.xpath('//*[@id="job-content"]'):
-            for job in response.xpath('//*[@id="job-content"]'):
-                # import pdb; pdb.set_trace()
-                yield {
-                    'title': job.css('font::text').extract_first(),
-                    'url': response.url,
-                    'company': job.css('span.company::text').extract_first(),
-                    'location': job.css('span.location::text').extract_first(),
-                    'summary': job.css('span.summary::text').extract_first(),
-                }
+                        for key in company_dict:
+                            yield {
+                                'title': company_dict[key]['title'],
+                                'url': company_dict[key]['url'],
+                                'company': company_dict[key]['company'],
+                                # location = element.css('span.location span::text').extract_first()
+                                # summary = element.css('span.summary::text').extract_first()
+                            }
+                            continue
+            elif response.xpath('//*[@id="job-content"]'):
+                for job in response.xpath('//*[@id="job-content"]'):
+                    # import pdb; pdb.set_trace()
+                    yield {
+                        'title': job.css('font::text').extract_first(),
+                        'url': response.url,
+                        'company': job.css('span.company::text').extract_first(),
+                        'location': job.css('span.location::text').extract_first(),
+                        'summary': job.css('span.summary::text').extract_first(),
+                    }
+
 
         next_page = response.css('div.pagination a::attr(href)').extract_first()
         if next_page is not None:
