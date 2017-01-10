b98bc528c4f41dcae728222cca975db7af3bbd02
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-09 20:13:51 -0800

    adding pagination crawl

--- octojobs/octojobs/octopus/octopus/spiders/spider.py
+++ octojobs/octojobs/octopus/octopus/spiders/spider.py
@@ -25,19 +25,50 @@ class JobSpider(scrapy.Spider):
         if not response.xpath('//*[@id="job-content"]'):
             if response.xpath('//*[@id="resultsCol"]'):
                 url_list = []
+                title_list = []
+                company_list = []
+
                 for element in response.css('div.result h2.jobtitle'):
+                    # import pdb;pdb.set_trace()
                     anchor = element.css('a.turnstileLink').extract_first()
+                    title = re.search(r'title="([^"]*)"', anchor).group(1)
+                    title_list.append(title)
+
                     url = re.search(r'href="([^"]*)"', anchor).group(1)
                     base_url = 'https://www.indeed.com'
                     url_list.append(base_url + url)
+
+                for data in response.css('div.result span.company'):
+                    if data.css('span a'):
+                        company = data.css('span a::text').extract_first()
+                    company = data.css('span::text').extract_first()
+                    company_list.append(company)
+
                 for url in url_list:
                     temp_url = ""
+                    temp_title = ""
+                    temp_company = ""
+
                     temp_url = url
+                    temp_title = title
+                    temp_company = company
+
                     url_list.pop(0)
+                    title_list.pop(0)
+                    company_list.pop(0)
                     yield scrapy.Request(temp_url)
 
                     if not response.xpath('//*[@id="job-content"]'):
-                        yield scrapy.Request(base_url)
+                        yield {
+                            'title': temp_title,
+                            'url': temp_url,
+                            'company': temp_company,
+                            # location = element.css('span.location span::text').extract_first()
+                            # summary = element.css('span.summary::text').extract_first()
+                            # anchor = element.css('h2.jobtitle a.turnstileLink').extract_first()
+                        }
+                        continue
+                    yield scrapy.Request(base_url)
             else:
                 self.parse(response)
         elif response.xpath('//*[@id="job-content"]'):
@@ -50,3 +81,8 @@ class JobSpider(scrapy.Spider):
                     'location': job.css('span.location::text').extract_first(),
                     'summary': job.css('span.summary::text').extract_first(),
                 }
+
+        next_page = response.css('div.pagination a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
