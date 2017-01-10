ce61832f8b2cfaed6ca0bbe768f18d70c2827285
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-10 09:24:54 -0800

    added view for results page

--- octojobs/octojobs/views/default.py
+++ octojobs/octojobs/views/default.py
@@ -1,7 +1,9 @@
-from pyramid.response import Response
+"""Render views from configurations."""
+
+# from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
+from pyramid.httpexceptions import HTTPFound
 
 from ..models import Job
 
@@ -9,36 +11,31 @@ from ..models import Job
 @view_config(route_name='home', renderer='../templates/home.jinja2')
 def home_view(request):
     """On initial load, shows search bar. On query submit, loads results."""
-    # If user presses submit button, load db query results:
-    # try
-        if request.method == 'POST':
-            searchterm = request.POST['searchbar'] + '%'
-            query = session.query(Job).filter(Job.city.like(searchterm))
+    if request.method == 'POST':
 
-    # except DBAPIError:
-    #     return Response(db_err_msg, content_type='text/plain', status=500)
-    # Else, display searchbar
+        searchterm = request.POST['searchbar']
+
+        # url = request.route_url('idea', idea='great', _query={'sort':'asc'})
+
+        # url = '/results?search=' + searchterm
+
+        return HTTPFound(location=request.route_url('results', _query={'search': searchterm}))
     return {}
+
+
+@view_config(route_name='results', renderer='../templates/results.jinja2')
+def result_view(request):
+    """Generate result view."""
+    # if request.method == 'GET':
+    import pdb; pdb.set_trace()
+
+    searchterm = request.GET.get('search') + '%'
+    query = request.dbsession.query(Job).filter(Job.city.like(searchterm))
+
+    return {'results': query}
 
 
 @view_config(route_name='about', renderer='../templates/about.jinja2')
 def about_view(request):
-    """Shows basic about us view."""
+    """Show basic about us view."""
     return {}
-
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_octojobs_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
