29ede1ef0a97903110f32bd5f4600a7c8fde9544
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-11 14:18:26 -0800

    expanding spider to include searches to dice.com.

--- octojobs/octojobs/octopus/octopus/spiders/spider.py
+++ octojobs/octojobs/octopus/octopus/spiders/spider.py
@@ -5,7 +5,21 @@ from octopus.items import OctopusItem
 
 
 class JobSpider(scrapy.Spider):
-    """Job Spider which will crawl the start_urls.
+    """Crawl each url in start_urls and extract data.
+    Data will be ported into pipelines as OctopusItem instances.
+    Pipelines will find each item instance and pass it to the database.
+
+    create_dict(self, iter, url, title, company, city, description) creates and
+    returns a new empty dictionary for each job site and populates with data
+    pulled by using css selectors and x-paths.
+
+    build_items(self, items, iter, key) takes the dictionaries created, and
+    creates and returns OctopusItem instances from them.
+
+    parse(self, response) is the default callback method for the Spider class.
+    It takes in the HTTP response, pulls data from html tags using CSS/ x-path
+    selectors, and uses that data to create item instances. The pipelines.py
+    file takes in those item instances and ports them to the database.
 
     And run the parse method on each of them.
     """
@@ -13,6 +27,8 @@ class JobSpider(scrapy.Spider):
     name = "jobs"
 
     start_urls = [
+        # 'file:///Users/rachaelwisecarver/codefellows/401/octojobs/Octojobs/octojobs/octojobs/octopus/indeed_list_view.html'
+        # 'file:///Users/rachaelwisecarver/codefellows/401/octojobs/Octojobs/octojobs/octojobs/octopus/dice_list_view.html',
         'https://www.indeed.com/jobs?q=python&l=seattle%2C+wa',
         'https://www.indeed.com/jobs?q=javascript&l=seattle%2C+wa',
         'https://www.indeed.com/jobs?q=ios&l=seattle%2C+wa',
@@ -34,86 +50,133 @@ class JobSpider(scrapy.Spider):
         'https://www.indeed.com/jobs?q=front+end&l=New+York%2C+NY',
         'https://www.indeed.com/jobs?q=full+stack&l=New+York%2C+NY',
         'https://www.indeed.com/jobs?q=data+scientist&l=New+York%2C+NY',
+        'https://www.dice.com/jobs?q=&l=seattle%2C+WA',
+        'https://www.dice.com/jobs?q=&l=San+Francisco+Bay+Area%2C+CA',
+        'https://www.dice.com/jobs?q=&l=New+York%2C+NY',
     ]
 
+    def create_dict(self, iter, url, title, company, city, description):
+        """Takes in an empty dictionary, and the items pulled from html.
+        Creates a new dictionary within that dictionary holding these items."""
+        iter[url] = {}
+        iter[url]['url'] = url
+        iter[url]['title'] = title
+        iter[url]['company'] = company
+        iter[url]['city'] = city
+        iter[url]['description'] = description
+        return iter
+
+    def build_items(self, items, iter, key):
+        """Takes in a dictionary and a dict key.
+        Returns an instance of the item class, to be passed to pipelines."""
+
+        items[key] = OctopusItem(
+                        title=iter[key]['title'],
+                        url=iter[key]['url'],
+                        company=iter[key]['company'],
+                        city=iter[key]['city'],
+                        description=iter[key]['description'],
+                    )
+        return items
+
     def parse(self, response):
-        """Default callback used by Scrapy to process downloaded responses.
-
-        If the spider is on a home page with a list view, build a dictionary.
-        This dictionary will contain references to each url, and the
-        associated job title, company name, location, and summary.
-        It will then use that url in its callback to the parse method, which
-        lets it follow that link to what should be a detail view. As of this
-        commit, all data is saved in a unique file as JSON.
-
-        Once the spider is on a detail view, it captures the CSS/ xPath
-        selectors for the data we want to port to our database. The spider then
-        returns to the home view and runs parse() again.
-
-        Once the spider reaches the end of the urls on the page, it will follow
-        the link to the next page of results.
-        """
+        """Default callback used by Scrapy to process downloaded responses."""
         items = {}
         company_dict = {}
-        if not response.xpath('//*[@id="job-content"]'):
-            if response.xpath('//*[@id="resultsCol"]'):
+        dice_company_dict = {}
 
-                for element in response.css('div.result h2.jobtitle'):
-                    anchor = element.css('a.turnstileLink').extract_first()
-                    title = re.search(r'title="([^"]*)"', anchor).group(1)
+        if response.xpath('//*[@id="resultsCol"]'):
+            """Search through Indeed list view for links to jobs."""
 
-                    base_url = 'https://www.indeed.com'
-                    url = base_url + re.search(
-                        r'href="([^"]*)"', anchor).group(1)
-                    company_dict[url] = {}
-                    company_dict[url]['url'] = url
-                    company_dict[url]['title'] = title
+            for element in response.css('div.result h2.jobtitle'):
+                """Grab items using css selectors and x-paths."""
 
-                    data = response.css('div.result span.company')
-                    if data.css('span a'):
-                        company = data.css('span a::text').extract_first()
-                    else:
-                        company = data.css('span::text').extract_first()
-                    company = ' '.join(company.split())
-                    company_dict[url]['company'] = company
+                anchor = element.css('a.turnstileLink').extract_first()
+                title = re.search(r'title="([^"]*)"', anchor).group(1)
+                base_url = 'https://www.indeed.com'
+                url = base_url + re.search(
+                    r'href="([^"]*)"', anchor).group(1)
+                data = response.css('div.result span.company')
+                if data.css('span a'):
+                    company = data.css('span a::text').extract_first()
+                else:
+                    company = data.css('span::text').extract_first()
+                company = ' '.join(company.split())
+                city = response.css(
+                    'div.result span.location::text').extract_first()
+                description = response.css(
+                    'div.result span.summary::text').extract_first()
 
-                    location = response.css(
-                        'div.result span.location::text').extract_first()
-                    company_dict[url]['city'] = location
+                """Set up dictionary for Indeed job info"""
+                self.create_dict(
+                    company_dict,
+                    url,
+                    title,
+                    company,
+                    city,
+                    description)
 
-                    summary = response.css(
-                        'div.result span.summary::text').extract_first()
-                    company_dict[url]['description'] = summary
+            """Follow each link, and build items to pass to pipeline."""
+            for key in company_dict:
+                yield scrapy.Request(key)
 
-                for key in company_dict:
-                    yield scrapy.Request(key)
+                if not response.xpath('//*[@id="job-content"]'):
+                    self.build_items(items, company_dict, key)
+                    continue
 
-                    if not response.xpath('//*[@id="job-content"]'):
-                        items[key] = OctopusItem(
-                            title=company_dict[key]['title'],
-                            url=company_dict[key]['url'],
-                            company=company_dict[key]['company'],
-                            city=company_dict[key]['city'],
-                            description=company_dict[key]['description'],
-                        )
-                        continue
+                else:
+                    company_dict[key]['description'] = response.css(
+                        'span.summary::text').extract_first()
+                    self.build_items(items, company_dict, key)
+                    continue
 
-            elif response.xpath('//*[@id="job-content"]'):
-                for job in response.xpath('//*[@id="job-content"]'):
-                    items[response.url] = OctopusItem(
-                        title=job.css('font::text').extract_first(),
-                        url=response.url,
-                        company=job.css(
-                            'span.company::text').extract_first(),
-                        city=job.css(
-                            'span.location::text').extract_first(),
-                        description=job.css(
-                            'span.summary::text').extract_first(),
-                    )
+            """Find the next page of job postings. Go there and call parse."""
+            next_page = response.css(
+                'div.pagination a::attr(href)').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
 
-        next_page = response.css(
-            'div.pagination a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        elif response.xpath('//*[@id="resultSec"]'):
+            """If the site is dice.com - do the same thing as above."""
+
+            for element in response.css('div.serp-result-content'):
+                """Grab items using css selectors and x-paths."""
+                # import pdb;pdb.set_trace()
+                anchor = element.css('a.dice-btn-link').extract_first()
+                # title = re.search(r'title="([^"]*)"', anchor).group(1)
+                title = element.css(
+                    'a.dice-btn-link.loggedInVisited::text').extract_first()
+                url = re.search(
+                    r'href="([^"]*)"', anchor).group(1)
+                company = element.css(
+                    'ul.list-inline a.dice-btn-link::text').extract_first()
+                description = ' '.join(element.css(
+                    'div.shortdesc::text').extract_first().split())
+                city = element.css(
+                    'ul.list-inline li.location::text').extract_first()
+
+                """Set up dictionary for Dice job info"""
+                self.create_dict(
+                    dice_company_dict,
+                    url,
+                    title,
+                    company,
+                    city,
+                    description)
+
+            """Follow each link, and build items to pass to pipeline."""
+            for key in dice_company_dict:
+                yield scrapy.Request(key)
+
+                if not response.xpath('//*[@id="bd"]'):
+                    self.build_items(items, dice_company_dict, key)
+                    continue
+
+                else:
+                    dice_company_dict[key]['description'] = response.css(
+                        'div.highlight-black::text').extract_first()
+                    self.build_items(items, dice_company_dict, key)
+                    continue
+
         yield items
