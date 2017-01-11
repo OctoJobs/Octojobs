e698008a701c8d51992a185569df9f4201383e57
Colin Lamont <colinlamont@gmail.com>
2017-01-11 12:35:25 -0800

    working search: both fields, just search, or just location.

--- octojobs/routes.py
+++ octojobs/routes.py
@@ -2,7 +2,11 @@
 
 
 def includeme(config):
+    """The module defines the routes that tie in to the views on defaul.py."""
     config.add_static_view(name='static', path='octojobs:static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('about', '/about')
     config.add_route('results', '/results')
+    config.add_route('no_search', '/no_search')
+    config.add_route('no_results', '/no_results')
+    config.add_route('404', '/404')
