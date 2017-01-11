4f3f66f6b542c12953ea96fceb394c49c600f72b
Claire Gatenby <clairejgatenby@gmail.com>
2017-01-11 13:20:20 -0800

    Revert "Search"

--- octojobs/tests/tests.py
+++ octojobs/tests/tests.py
@@ -1,127 +1,65 @@
-"""The test module for the Octojobs project."""
-
+import unittest
 import transaction
-import pytest
-from octojobs.models import Job, get_tm_session
-from octojobs.models.meta import Base
-import faker
+
 from pyramid import testing
 
 
-fake = faker.Faker()
-
-JOBS = [Job(
-    title=fake.job(),
-    city=fake.city(),
-    description=fake.text(100),
-    empoyer=fake.company(),
-) for i in range(10)]
-
-
-@pytest.fixture(scope="session")
-def configuration(request):
-    """Set up a Configurator instance.
-
-    This Configurator instance sets up a pointer to the location of the database.
-    It also includes the models from your app's model package.
-    Finally it tears everything down, including the in-memory database.
-
-    This configuration will persist for the entire duration of your PyTest run.
-    """
-    settings = {
-        'sqlalchemy.url': 'postgres:///test_jobs'}
-    config = testing.setUp(settings=settings)
-    config.include('octojobs.models')
-    config.include('octojobs.routes')
-
-    def teardown():
-        testing.tearDown()
-
-    request.addfinalizer(teardown)
-    return config
-
-
 def dummy_request(dbsession):
-    """It creates a fake HTTP Request and database session.
-
-    This is a function-level fixture, so every new request will have a
-    new database session.
-    """
     return testing.DummyRequest(dbsession=dbsession)
 
 
-@pytest.fixture
-def add_models(dummy_request):
-    """Add a bunch of model instances to the database.
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp(settings={
+            'sqlalchemy.url': 'sqlite:///:memory:'
+        })
+        self.config.include('.models')
+        settings = self.config.get_settings()
 
-    Every test that includes this fixture will add jobs.
-    """
-    dummy_request.dbsession.add_all(JOBS)
+        from .models import (
+            get_engine,
+            get_session_factory,
+            get_tm_session,
+            )
 
+        self.engine = get_engine(settings)
+        session_factory = get_session_factory(self.engine)
 
-@pytest.fixture
-def db_session(configuration, request):
-    """Create a session for interacting with the test database.
+        self.session = get_tm_session(session_factory, transaction.manager)
 
-    This uses the dbsession_factory on the configurator instance to create a
-    new database session. It binds that session to the available engine
-    and returns a new session for every call of the dummy_request object.
-    """
-    SessionFactory = configuration.registry['dbsession_factory']
-    session = SessionFactory()
-    engine = session.bind
-    Base.metadata.create_all(engine)
+    def init_database(self):
+        from .models.meta import Base
+        Base.metadata.create_all(self.engine)
 
-    def teardown():
-        session.transaction.rollback()
+    def tearDown(self):
+        from .models.meta import Base
 
-    request.addfinalizer(teardown)
-    return session
-
-
-@pytest.fixture
-def testapp():
-    """The fixture creates a test app."""
-    from webtest import TestApp
-    from pyramid.config import Configurator
+        testing.tearDown()
+        transaction.abort()
+        Base.metadata.drop_all(self.engine)
 
 
-    def main(global_config, **settings):
-        """ The function returns a Pyramid WSGI application."""
-        config = Configurator(settings=settings)
-        config.include('pyramid_jinja2')
-        config.include('.models')
-        config.include('.routes')
-        config.scan()
-        return config.make_wsgi_app()
+class TestMyViewSuccessCondition(BaseTest):
 
-    app = main({}, **{"sqlalchemy.url": 'postgres://colinlamont@localhost:5432/octojobs'})
-    testapp = TestApp(app)
+    def setUp(self):
+        super(TestMyViewSuccessCondition, self).setUp()
+        self.init_database()
 
-    SessionFactory = app.registry["dbsession_factory"]
-    engine = SessionFactory().bind
-    Base.metadata.create_all(bind=engine)
-    return testapp
+        from .models import Job
 
+        model = Job(name='one', value=55)
+        self.session.add(model)
 
-@pytest.fixture
-def fill_the_db(testapp):
-    """It fills the database."""
-    SessionFactory = testapp.app.registry["dbsession_factory"]
-    with transaction.manager:
-        dbsession = get_tm_session(SessionFactory, transaction.manager)
-        dbsession.add_all(Job)
+    def test_passing_view(self):
+        from .views.default import my_view
+        info = my_view(dummy_request(self.session))
+        self.assertEqual(info['one'].name, 'one')
+        self.assertEqual(info['project'], 'octojobs')
 
 
-def test_search_returns_results(testapp, fill_the_db):
-    """When there's data in the database, the home page has rows."""
-    response = testapp.get('results', method="POST", location="Seattle")
-    html = response.html
-    assert html.find_all("Windows") is True
+class TestMyViewFailureCondition(BaseTest):
 
-
-def test_new_jobs_are_added(db_session):
-    """New jobs get added to the database."""
-    db_session.add_all(JOBS)
-    query = db_session.query(Job).all()
-    assert len(query) == len(JOBS)
+    def test_failing_view(self):
+        from .views.default import my_view
+        info = my_view(dummy_request(self.session))
+        self.assertEqual(info.status_int, 500)
--- octojobs/views/default.py
+++ octojobs/views/default.py
@@ -1,7 +1,6 @@
 """Render views from configurations."""
 
 from pyramid.view import view_config
-from sqlalchemy import or_, and_
 
 from pyramid.httpexceptions import HTTPFound
 
@@ -12,31 +11,12 @@ from ..models import Job
 def home_view(request):
     """On initial load, shows search bar. On query submit, loads results."""
     if request.method == 'POST':
-        # import pdb; pdb.set_trace()
-        # if request.POST['searchbar']:
+
         searchterm = request.POST['searchbar']
-        # if request.POST['location']:
-        location = request.POST['location']
 
-        if not location and not searchterm :
-            return HTTPFound(
-                location=request.route_url('home')
-            )
-
-        elif not location and searchterm:
-            return HTTPFound(
-                location=request.route_url('results', _query={'search': searchterm})
-            )
-
-        elif not searchterm and location:
-            return HTTPFound(
-                location=request.route_url('results', _query={'location': location})
-            )
-
-        elif searchterm and location:
-            return HTTPFound(
-                location=request.route_url('results', _query={'search': searchterm, 'location': location})
-            )
+        return HTTPFound(
+            location=request.route_url('results', _query={'search': searchterm})
+        )
 
     return {}
 
@@ -45,54 +25,17 @@ def home_view(request):
 def result_view(request):
     """Generate result view."""
 
-    import pdb; pdb.set_trace()
-    if request.GET.get('search'):
-        searchterm = '%' + request.GET.get('search') +'%'
-    else:
-        searchterm = None
-
-    if request.GET.get('location'):
-        location = '%' + request.GET.get('location') +'%'
-    else:
-        location = None
-    # query = request.dbsession.query(Job).filter(Job.city.ilike(searchterm)).first()
-
-    field_category = [Job.title, Job.company, Job.description]
-
-    if request.method == 'GET':
-        if location and searchterm:
-            print("search started")
-            for field in field_category:
-                if request.dbsession.query(Job).filter(and_(Job.city.ilike(location), field.ilike(searchterm))):
-                    query = request.dbsession.query(Job).filter(and_(Job.city.ilike(location), field.ilike(searchterm)))
-                    break
-
-        elif searchterm and location is None:
-            field_category.append(Job.city)
-            for field in field_category:
-                if request.dbsession.query(Job).filter(field.ilike(searchterm)):
-                    query = request.dbsession.query(Job).filter(field.ilike(searchterm))
-                    break
-
-        elif location and searchterm is None:
-            if request.dbsession.query(Job).filter(Job.city.ilike(location)):
-                query = request.dbsession.query(Job).filter(Job.city.ilike(location))
-
-        elif location is None and searchterm is None:
-            return HTTPFound(location=request.route_url('no_search'))
-
-        else:
-            return HTTPFound(location=request.route_url('no_results'))
+    searchterm = '%' + request.GET.get('search') + '%'
+    query = request.dbsession.query(Job).filter(Job.city.ilike(searchterm)).first()
 
     if request.method == 'POST':
 
         searchterm = request.POST['searchbar']
-        location = request.POST['location']
 
         return HTTPFound(
-            location=request.route_url('results', _query={'search': searchterm, 'location': location})
+            location=request.route_url('results', _query={'search': searchterm})
         )
-    # import pdb; pdb.set_trace()
+
     return {'results': query}
 
 
