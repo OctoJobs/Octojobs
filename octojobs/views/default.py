527082bc4e47b496c1f9feebf97304648c344233
Colin Lamont <colinlamont@gmail.com>
2017-01-11 13:47:38 -0800

    removed pdb from default.

--- octojobs/views/default.py
+++ octojobs/views/default.py
@@ -45,7 +45,7 @@ def home_view(request):
 def result_view(request):
     """Generate result view."""
 
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     if request.GET.get('search'):
         searchterm = '%' + request.GET.get('search') +'%'
     else:
